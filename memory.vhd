b22db8952825beab2803ded73578dad7b7c05ad7
ChengZhiZhao <52762910+ChengZhiZhao420@users.noreply.github.com>
2021-12-01 15:55:03 -0800

    Update memory.vhd

--- memory.vhd
+++ memory.vhd
@@ -16,24 +16,43 @@ entity memory is
 end memory;
 
 architecture behavior of memory is	
-type memory is array (0 to 13) of std_logic_vector(0 to 7); 
+type memory is array (0 to 31) of std_logic_vector(0 to 7); 
 signal index : integer;
 signal data : memory:=(
+x"01",
+x"28",
+x"50",
+x"24",-------and $t2, $t1, $t0
+x"01",
+x"28",
+x"50",
+x"20",-------add $t2, $t1, $t0
+x"8E",
+x"11",
+x"00",
+x"01",-------lw $17, 1($16)
+x"01",
+x"28",
+x"50",
+x"22",--------sub $t5, $t1, $t0
+x"11",
+x"09",
+x"01",
+x"00",--------Beq $8, $9, 256
+x"AE",
+x"30",
+x"00",
+x"16",--------sw $16, 22($17)
+x"01",
+x"4B",
+x"48",
+x"25",--------or $9, $10, $11
 x"21",
-x"0A",
-x"BF",
-x"2A",
-x"04",
-x"23",
-x"78",
-x"0F",
-x"FF",
-x"22",
-x"33",
-x"44",
-x"52",
-x"F1"
+x"49",
+x"00",
+x"0C"--------addi $t1, $t2, 12
 );
+
 signal count :integer:= 0;
 begin	 
 	process(clk)
