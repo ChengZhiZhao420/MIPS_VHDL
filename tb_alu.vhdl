7db788e7e5d12fe5678b530bd6c525641c544679
ideniaa <55167367+ideniaa@users.noreply.github.com>
2021-12-06 15:50:30 -0800

    Update tb_alu.vhdl

--- tb_alu.vhdl
+++ tb_alu.vhdl
@@ -1,1 +1,43 @@
---Idenia Ayala
+library ieee;
+use ieee.std_logic_1164.all;
+
+architecture behav of tb_alu is
+	--inputs
+	signal tb_a : std_logic_vector(31 downto 0) := (others => '0');
+	signal tb_b : std_logic_vector(31 downto 0) := (others => '0');
+	signal tb_control : std_logic_vector(3 downto 0) := (others => '0');
+	--outputs
+	signal tb_result : std_logic_vector(31 downto 0);
+	signal tb_zero : std_logic;
+
+begin
+	uu_test : entity work.ALU(behavioral)
+		port map( a => tb_a,
+		b => tb_b,
+		control => tb_control,
+		result => tb_result,
+		zero => tb_zero
+		);
+-- stimulus
+stim : process
+begin
+	tb_a <= x"00000003";
+	tb_b <= x"FFFFFFFF";
+	tb_control <= "0000"; -- add
+	wait for 50ns;
+	tb_control <= "0001"; -- sub
+	wait for 50ns;
+	tb_control <= "0010"; -- and
+	wait for 50ns;
+	tb_control <= "0011"; -- or
+	wait for 50ns;
+	tb_control <= "0100"; -- xor
+	wait for 50ns;
+	tb_control <= "0101"; -- set on less than
+	wait for 50ns;
+
+	assert false
+	report "end"
+	severity failure;
+end process;
+end;
