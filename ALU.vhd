02de6ec8b83d23725ebdb7a62e7c5641c588221a
ChengZhiZhao <52762910+ChengZhiZhao420@users.noreply.github.com>
2021-12-06 15:05:56 -0800

    Update ALU.vhd

--- ALU.vhd
+++ ALU.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 
 entity alu is
 	port ( a, b : in std_logic_vector(31 downto 0); --src1, src2
-	alu_control : in std_logic_vector(3 downdto 0); -- function select
+	alu_control : in std_logic_vector(3 downto 0); -- function select
 	alu_result : out std_logic_vector(31 downto 0); --output result
 	zero : out std_logic -- zero flag
 	);
@@ -18,28 +18,31 @@ begin
 	begin
 		case alu_control is
 			when "0000" =>
-				result <= a and b; -- and
+				result <= std_logic_vector(signed(a) + signed(b)); -- add
 			when "0001" =>
-				result <= a or b; -- or
-			when "0010" =>
-				result <= std_logic_vector(signed(a)) + signed(b)); -- add
-			when "0110" =>
 				result <= std_logic_vector(signed(a) - signed(b)); -- sub
-			when "01111" =>		-- set on less than
+			when "0010" =>
+				result <= a and b; -- and
+			when "0011" =>
+				result <= a or b; -- or
+			when "0100" =>
+				result <= a xor b; -- xor
+			when "0101" =>		-- set on less than
 				if (signed(a) < signed(b)) then
 					result <= x"00000001";
 				else
 					result <= x"00000000";
 				end if;
-			when "1100" =>
-				result => a xor b; -- xor
-			when others => NULL
-				result => x"00000000";
+			when others => 
+				result <= x"00000000";
 		end case;
 
-	zero <= '1' when result = x"00000000" else "0";
+	if(result = x"00000000") then
+		zero <= '1';
+	else
+		zero <= '0';
+	end if;
 	alu_result <= result;
 	end process;
 end behav;
-
 
