d1a6e53b4c1562f155d39833d879e4e8e858ec09
ChengZhiZhao <52762910+ChengZhiZhao420@users.noreply.github.com>
2021-12-06 23:53:23 -0800

    Update programCounter.vhd

--- programCounter.vhd
+++ programCounter.vhd
@@ -3,7 +3,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity programCount is 
-	port(programCountIn :in std_logic_vector(0 to 7);
+	port(programCountIn :in std_logic_vector(0 to 7):= "00000000";
 	clk :in std_logic;
 	programCountOut :out std_logic_vector(0 to 7));
 end programCount;
