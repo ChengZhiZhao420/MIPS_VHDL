948276f0b99cea9b64d41ac90e1e20a2a08d1088
ideniaa <55167367+ideniaa@users.noreply.github.com>
2021-12-09 13:44:38 -0800

    Update ALU.vhd

--- ALU.vhd
+++ ALU.vhd
@@ -3,33 +3,38 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity alu is
+entity ALU is
 	port ( a, b : in std_logic_vector(31 downto 0); --src1, src2
 	alu_control : in std_logic_vector(3 downto 0); -- function select
 	clk : in std_logic;
 	alu_result : out std_logic_vector(31 downto 0); --output result
 	zero : out std_logic -- zero flag
 	);
-end alu;
+end ALU;
 
-architecture behav of alu is
+architecture behav of ALU is
 	signal result : std_logic_vector (31 downto 0);
 begin
 	process (clk)
 	begin
 		if(rising_edge(clk)) then
 			case alu_control is
-				when "0010" =>
-					result <= std_logic_vector(signed(a) + signed(b)); -- add
-				when "0110" =>
-					result <= std_logic_vector(signed(a) - signed(b)); -- sub
 				when "0000" =>
-					result <= a and b; -- and
+					result <= std_logic_vector(signed(a) + signed(b)); -- add
 				when "0001" =>
+					result <= std_logic_vector(signed(a) - signed(b)); -- sub
+				when "0010" =>
+					result <= a and b; -- and
+				when "0011" =>
 					result <= a or b; -- or
 				when "0100" =>
 					result <= a xor b; -- xor
-				when "0111" =>		-- set on less than
+				--shift left
+				when "0101" => result <= std_logic_vector(shift_left(unsigned(a),to_integer(unsigned(b(10 downto 6)))));
+				 --shift right 
+        		        when "0110" => result <= std_logic_vector(shift_right(unsigned(a),to_integer(unsigned(b(10 downto 6)))));
+				-- set on less than
+				when "0111" =>		
 					if (signed(a) < signed(b)) then
 						result <= x"00000001";
 					else
@@ -48,4 +53,3 @@ begin
 		end if;
 	end process;
 end behav;
-
